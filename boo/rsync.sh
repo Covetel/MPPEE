ff5cf9786955742e0ae5c1d1c54b32f2806a417b
Walter Vargas <walter@covetel.com.ve>
2013-08-13 18:16:07 -0430

    adding files of boo server

---
+++ boo/rsync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rsync -avzr -e ssh root@boo.mppee.gob.ve:/etc/bind/ etc/bind/
+rsync -avzr -e ssh root@boo.mppee.gob.ve:/etc/bind/ etc/bind/
+rsync -avzr -e ssh root@boo.mppee.gob.ve:/var/log/syslog var/log/syslog
+rsync -avzr -e ssh root@boo.mppee.gob.ve:/var/log/syslog.1.gz var/log/syslog.1.gz
+rsync -avzr -e ssh root@boo.mppee.gob.ve:/var/log/syslog.2.gz var/log/syslog.2.gz
+rsync -avzr -e ssh root@boo.mppee.gob.ve:/var/log/syslog.3.gz var/log/syslog.3.gz
+rsync -avzr -e ssh root@boo.mppee.gob.ve:/var/log/syslog.4.gz var/log/syslog.4.gz
