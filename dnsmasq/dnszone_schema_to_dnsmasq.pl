080bfd162973369f4470b0a99d434e047c906473
Carlos Paredes <cparedes@covetel.com.ve>
2013-08-22 14:33:00 -0430

    Script para convertir LDIF de dnszone.schema a DNSMASQ #8
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

---
+++ dnsmasq/dnszone_schema_to_dnsmasq.pl
@@ -0,0 +1,132 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use v5.14;
+use Net::LDAP::LDIF;
+use Data::Dumper;
+
+#neg-ttl = 1200
+#ptr-record=
+#address=
+#cname=
+#mx-host
+
+my $file =  shift || "/home/aphu/Documents/Covetel/MPPEE/MPPEE-Covetel/dns.ldif";
+
+my $hosts = "/etc/hosts";
+my $dnsmasq = "/etc/dnsmasq.conf";
+
+my $ldif = Net::LDAP::LDIF->new( $file, "r",  onerror => 'die' );
+
+while ( not $ldif->eof() ) {
+    open HOST, ">>", $hosts || die "Error $!"; 
+    open DNS, ">>", $dnsmasq || die "Error $!"; 
+
+    my @h;
+
+    my $entrys =  $ldif->read_entry();
+
+    foreach my $entry ( $entrys ) {
+            if ( $entry->get_value("arecord") ) { 
+                print HOST &pointer($entry)."\n"; 
+            }
+            if ( $entry->get_value("dnsttl") ) { 
+                print DNS &dnsttl($entry)."\n"; 
+            }
+            if ( $entry->get_value("mxrecord") ) { 
+                print DNS &mxrecord($entry)."\n"; 
+            }
+            if ( $entry->get_value("cnamerecord") ) { 
+                print DNS &cnamerecord($entry)."\n"; 
+            }
+            if ( $entry->get_value("txtrecord") ) { 
+                print DNS &txtrecord($entry)."\n"; 
+            }
+            if ( $entry->get_value("ptrrecord") ) { 
+                print DNS &ptrrecord($entry)."\n"; 
+            }
+    }
+
+    close HOST;
+    close DNS;
+}
+
+ub pointer {
+    my $entry = shift;
+
+    my $pointer = $entry->get_value("arecord").
+        " ".
+        $entry->get_value("relativedomainname").
+        ".".
+        $entry->get_value("zonename").
+        " ".
+        $entry->get_value("relativedomainname");
+
+    return $pointer;
+}
+
+sub mxrecord {
+    my $entry = shift;
+
+    my ($preference, $host) = split(" ", $entry->get_value("mxrecord"));
+
+    $host =~ s/\.$//g;
+
+    my $mxrecord = "mx-host=".
+        $host.
+        ",".
+        $entry->get_value("zonename").
+        ",".
+        $preference;
+
+    return $mxrecord;
+}
+
+sub cnamerecord {
+    my $entry = shift;
+
+    my $cnamerecord = "cname=".
+        $entry->get_value("cnamerecord").
+        ",".
+        $entry->get_value("relativedomainname");
+
+    return $cnamerecord;
+}
+
+sub txtrecord {
+    my $entry = shift;
+
+    my $txtrecord = "txt-record=".
+        $entry->get_value("zonename").
+        ",".
+        $entry->get_value("txtrecord");
+
+    return $txtrecord;
+}
+
+sub ptrrecord {
+    my $entry = shift;
+
+    my $ptr = $entry->get_value("ptrrecord");
+    $ptr =~ s/\.$//g;
+
+    my $zone = $entry->get_value("zonename");
+    $zone =~ s/$/\./g;
+
+    my $ptrrecord = "ptr-record=".
+        $zone.
+        ",".
+        "\"".
+        $ptr.
+        "\"";
+
+    return $ptrrecord;
+}
+
+sub dnsttl {
+    my $entry = shift;
+
+    my $dnsttl = "neg-ttl=".$entry->get_value("dnsttl");
+
+    return $dnsttl;
+}
